5e068908096376e28040c86bc6d60afd1b22fe9c
Yevhen Oliynyk <i.yevhen.oliynyk@gmail.com>
2025-03-09 20:44:03 +0100

    Fix log

--- src/services/firebase/firebaseConfig.ts
+++ src/services/firebase/firebaseConfig.ts
@@ -1,7 +1,6 @@
 // src/lib/firebaseConfig.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-console.log(import.meta.env);
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
